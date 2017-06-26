e189c0b6d4f2400eef80d60c65552fc4a96f0a00
Robbie Averill <robbie@silverstripe.com>
2017-06-26 18:05:35 +1200

    Apply PSR-2 linting, separate FAQPage_Controller, add editorconfig and fix some doc blocks

--- code/admin/FAQAdmin.php
+++ code/admin/FAQAdmin.php
@@ -5,30 +5,33 @@
  * Allows a content author to publish and edit questions and answers.
  * @see FAQ for FAQ DataObject.
  */
-class FAQAdmin extends ModelAdmin {
+class FAQAdmin extends ModelAdmin
+{
+    private static $url_segment = 'faq';
 
-	private static $url_segment = 'faq';
+    private static $managed_models = array(
+        'FAQ'
+    );
 
-	private static $managed_models = array(
-		'FAQ'
-	);
+    private static $menu_title = 'FAQs';
 
-	private static $menu_title = 'FAQs';
-	
-	private static $model_importers = array(
-		'FAQ' => 'FAQCsvBulkLoader'
-	);
+    private static $model_importers = array(
+        'FAQ' => 'FAQCsvBulkLoader'
+    );
 
-	/**
-	 * Overload ModelAdmin->getExportFields() so that we can export keywords.
-	 * @see ModelAdmin::getExportFields
-	 */
-	public function getExportFields() {
-		return array(
-			'Question' => 'Question',
-			'Answer' => 'Answer',
-			'Keywords' => 'Keywords',
-			'Category.Name' => 'Category'
-		);
-	}
+    /**
+     * Overload ModelAdmin->getExportFields() so that we can export keywords.
+     *
+     * @see ModelAdmin::getExportFields
+     * @return array
+     */
+    public function getExportFields()
+    {
+        return array(
+            'Question' => 'Question',
+            'Answer' => 'Answer',
+            'Keywords' => 'Keywords',
+            'Category.Name' => 'Category'
+        );
+    }
 }
